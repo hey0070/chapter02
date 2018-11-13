17070f9742ca844debc08b0fbed280867837673a
BIT <BIT@DESKTOP-0VBACJC>
2018-11-13 21:51:27 +0900

    메소드 public / private 연습

---
+++ src/ex01/Goods.java
@@ -0,0 +1,18 @@
+package ex01;
+
+public class Goods {
+    private String name;
+    private int price;
+    
+   public void setName(String n) {
+	   name = n ;
+   }
+    
+   public void setPrice(int p) {
+	     price = p;   
+   
+   }
+    public void showinfo() {
+    	System.out.println("상품: " + name + "   가격: " + price);
+    }
+}
